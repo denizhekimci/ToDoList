8546dd7978566b7901e71ae2991a5d95b053de1e
DH <denizhekimci@gmail.com>
2016-06-18 13:27:59 +0300

    Slots for Mark as Done and Mark as Undone added.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,5 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QVector>
 #include <QtCore>
 #include <QtGui>
 #include <QMessageBox>
@@ -23,11 +22,17 @@ void MainWindow::AddRoot(Item item)
 void MainWindow::RemoveRoot()
 {
     QTreeWidgetItem *itm = ui->treeWidget->currentItem();
-    if(itm)
-        QMessageBox::question(this, "", "Are you sure you want to delete the item?", QMessageBox::Yes | QMessageBox::No);
-    int i = ui->treeWidget->indexOfTopLevelItem(itm);
-    ui->treeWidget->takeTopLevelItem(i);
-    delete itm;
+    if(itm){
+        QMessageBox::StandardButton reply;
+        reply = QMessageBox::question(this, "", "Are you sure you want to delete the item?", QMessageBox::Yes | QMessageBox::No);
+        if(reply == QMessageBox::Yes)
+        {
+            int i = ui->treeWidget->indexOfTopLevelItem(itm);
+            ui->treeWidget->takeTopLevelItem(i);
+            delete itm;
+            toDoList.removeAt(i);
+        }
+    }
 }
 
 void MainWindow::AddChild(QTreeWidgetItem *parent,Item item)
@@ -35,6 +40,11 @@ void MainWindow::AddChild(QTreeWidgetItem *parent,Item item)
     QTreeWidgetItem *itm = new QTreeWidgetItem();
     itm->setText(0,item.getEndDate().toString());
     parent->addChild(itm);
+}
+
+void MainWindow::moveTask(QTreeWidget *widget1, QTreeWidget *widget2, QList<Item> list1, QList<Item> list2)
+{
+
 }
 
 MainWindow::~MainWindow()
@@ -47,13 +57,43 @@ void MainWindow::on_pushButton_clicked()
     Item item;
     item.setDescription(ui->lineEdit->text());
     item.setEndDate(ui->dateEdit->date());
-    data.append(item);
+    toDoList.append(item);
     ui->treeWidget->setColumnCount(1);
-    AddRoot(data.last());
+    AddRoot(toDoList.last());
 }
 
 void MainWindow::on_pushButton_2_clicked()
 {
     RemoveRoot();
 
+}
+
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    QTreeWidgetItem *itm = ui->treeWidget->currentItem();
+
+    if(itm != NULL)
+    {
+        int i = ui->treeWidget->indexOfTopLevelItem(itm);
+        ui->treeWidget->takeTopLevelItem(i);
+        ui->treeWidget_2->addTopLevelItem(itm);
+
+        toDoList.removeAt(i);
+    }
+
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    QTreeWidgetItem *itm = ui->treeWidget_2->currentItem();
+
+    if(itm != NULL)
+    {
+        int i = ui->treeWidget_2->indexOfTopLevelItem(itm);
+        ui->treeWidget_2->takeTopLevelItem(i);
+        ui->treeWidget->addTopLevelItem(itm);
+
+        doneList.removeAt(i);
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -18,15 +18,22 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-    QList<Item> data;
+    QList<Item> toDoList;
+    QList<Item> doneList;
+
     void AddRoot(Item item);
     void AddChild(QTreeWidgetItem *parent,Item item);
     void RemoveRoot();
+    void moveTask(QTreeWidget *widget1, QTreeWidget *widget2, QList<Item> list1, QList<Item> list2);
 
 private slots:
     void on_pushButton_clicked();
 
     void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
 
 private:
     Ui::MainWindow *ui;
